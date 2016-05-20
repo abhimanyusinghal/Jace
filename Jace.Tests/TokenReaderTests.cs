db94fd907e79998bb457628a3cdce9ab4681fb0a
iMentor <abhimanyu.singhal@jabong.com>
2016-05-20 21:15:30 +0530

    Jace.iOS.Tests added

--- Jace.Tests/TokenReaderTests.cs
+++ Jace.Tests/TokenReaderTests.cs
@@ -5,7 +5,7 @@ using Jace.Tokenizer;
 
 #if NETFX_CORE
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-#elif __ANDROID__
+#elif __ANDROID__ || __IOS__
 using NUnit.Framework;
 using TestClass = NUnit.Framework.TestFixtureAttribute;
 using TestMethod = NUnit.Framework.TestAttribute;
